3278370ce525a2a376a3f5096ec26455c69c4b5b
Sreypovprak <sreypovprak88@gmail.com>
2024-09-08 14:12:52 +0700

    refactor(05-signup-api): refactore BaseController on Create function

--- utils/baseControllers.js
+++ utils/baseControllers.js
@@ -3,13 +3,14 @@ const AppError = require("./appError");
 const catchAsync = require("./catchAsync");
 
 /**
- *
- * @class BaseController : Crud default controller
- * @param Model : model name
+ * @class BaseController : CRUD default controller
+ * @param {Object} Model - Sequelize model
+ * @param {Array} [uniqueFields=[]] - Array of unique fields to check for uniqueness
  */
 class BaseController {
-    constructor(Model) {
+    constructor(Model, uniqueFields = []) {
         this.Model = Model;
+        this.uniqueFields = Array.isArray(uniqueFields) ? uniqueFields : [];
     }
 
     // Fetch all records
@@ -25,6 +26,16 @@ class BaseController {
     create = catchAsync(async (req, res, next) => {
         if (Object.keys(req.body).length === 0) {
             return next(new AppError("You can't create with empty field", 400));
+        }
+
+        // Check for existing fields
+        for (const field of this.uniqueFields) {
+            if (req.body[field]) {
+                const existingRecord = await this.Model.findOne({ where: { [field]: req.body[field] } });
+                if (existingRecord) {
+                    return next(new AppError(`A record with this ${field} already exists`, 409));
+                }
+            }
         }
 
         const newRecord = await this.Model.create(req.body);
@@ -64,7 +75,9 @@ class BaseController {
 
         // update the record
         await this.Model.update(req.body, {
-            where: { id },
+            where: {
+                id,
+            },
             validate: true,
             individualHooks: true,
         });
@@ -86,7 +99,11 @@ class BaseController {
             return next(new AppError("No record found with this ID", 404));
         }
 
-        await this.Model.destroy({ where: { id } });
+        await this.Model.destroy({
+            where: {
+                id,
+            },
+        });
         res.status(204).json({
             status: "success",
             data: null,
