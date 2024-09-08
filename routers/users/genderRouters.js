f8f6da3d14a459df8ec35961c672b72fbe726c14
Sreypovprak <sreypovprak88@gmail.com>
2024-09-08 18:42:57 +0700

    add(05-signup-api): add getOne, updateOne and deleteOne for gender controller and router

--- routers/users/genderRouters.js
+++ routers/users/genderRouters.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const genderControllers = require("../../controllers/users/genderControllers");
 
-router.route("/").get(genderControllers.getAll).post(genderControllers.createOne);
+router.route("/")
+    .get(genderControllers.getAll)
+    .post(genderControllers.createOne);
+    
+router
+    .route("/:id")
+    .get(genderControllers.getOne)
+    .patch(genderControllers.updateOne)
+    .delete(genderControllers.deleteOne);
 
 module.exports = router;
