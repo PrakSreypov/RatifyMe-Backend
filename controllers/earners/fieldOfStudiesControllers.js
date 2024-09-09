5e21baf25fd0661419ec90c3a36c2b48cba5f777
Khun Malen <malen8622@gmail.com>
2024-09-09 13:49:36 +0700

    feat(11-fieldOfStudies-api): start creating FieldOfStudies Controller

---
+++ controllers/earners/fieldOfStudiesControllers.js
@@ -0,0 +1,6 @@
+const FieldOfStudies = require("../../models/FieldOfStudies");
+const BaseControllers = require("../../utils/baseControllers");
+
+const FieldOfStudiesControllers = new BaseControllers(FieldOfStudies);
+
+module.exports = FieldOfStudiesControllers;
