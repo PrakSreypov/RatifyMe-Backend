4ab135acd7a946d93836ad2fe502523f7e9faa38
Khun Malen <malen8622@gmail.com>
2024-09-09 14:35:18 +0700

    add(11-fieldOfStudies-api): add fieldOfStudyRouters to the field of study api

--- routers/earners/index.js
+++ routers/earners/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const academicLevelRouters = require("./academicLevelRouters");
+const fieldOfStudyRouters = require("./fieldOfStudyRouters");
 
 router.use("/academiclevels", academicLevelRouters);
+router.use("/fieldofstudies", fieldOfStudyRouters);
 
 module.exports = router;
