03b8c134123fffecaf0d8e0faa85a6e23a16a4a9
Sreypovprak <sreypovprak88@gmail.com>
2024-09-09 10:15:18 +0700

    refactor(05-signup-api): refactor route position

--- routers/users/index.js
+++ routers/users/index.js
@@ -6,7 +6,6 @@ const roleRouters = require("./roleRouters");
 const genderRouters = require("./genderRouters");
 const addressRouters = require("./addressRoutes");
 
-router.use("/", userRouters);
 router.use("/roles", roleRouters);
 router.use("/genders", genderRouters);
 router.use("/addresses", addressRouters);
