a04a0602d488260c461fcd44b881aff00532c63d
Sreypovprak <sreypovprak88@gmail.com>
2024-09-10 11:46:50 +0700

    fix: resolve conflict

--- controllers/earners/fieldOfStudyControllers.js
+++ controllers/earners/fieldOfStudyControllers.js
@@ -1,7 +1,6 @@
-
 const FieldOfStudies = require("../../models/FieldOfStudies");
-
 const BaseControllers = require("../../utils/baseControllers");
+
 const FieldOfStudyControllers = new BaseControllers(FieldOfStudies);
 
 module.exports = FieldOfStudyControllers;
