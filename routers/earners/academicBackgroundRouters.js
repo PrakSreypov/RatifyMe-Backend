0e6b31f37f010077cbafd367c7eeccd3c88cff16
Khun Malen <malen8622@gmail.com>
2024-09-10 09:55:26 +0700

    add(15-earnersApi): start create academic background routers

---
+++ routers/earners/academicBackgroundRouters.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const academicBackgroundControllers = require("../../controllers/earners/academicBackgroundControllers");
+
+router
+    .route("/")
+    .get(academicBackgroundControllers.getAll)
+    .post(academicBackgroundControllers.createOne);
+
+router
+    .route("/:id")
+    .get(academicBackgroundControllers.getOne)
+    .patch(academicBackgroundControllers.updateOne)
+    .delete(academicBackgroundControllers.deleteOne);
+
+module.exports = router;
