de365a2ffd6fdeec683b6e56943d017bbfb6d392
Sreypovprak <sreypovprak88@gmail.com>
2024-09-11 18:47:26 +0700

    add(22-forgotPassword-api): add controller  handle on forgot password

--- controllers/auth/authControllers.js
+++ controllers/auth/authControllers.js
@@ -4,6 +4,7 @@ const BaseController = require("../../utils/baseControllers");
 const catchAsync = require("../../utils/catchAsync");
 const { createSendToken } = require("../../middlewares/auth");
 const AppError = require("../../utils/appError");
+const sendEmail = require("../../services/email");
 
 const Users = require("../../models/Users");
 const uniqueFields = ["email", "username", "phoneNumber"];
@@ -46,6 +47,48 @@ class AuthControllers extends BaseController {
         createSendToken(user, 200, res);
     });
     // ============ End Signin controller ============
+
+    // ============ Start Forgot Password controller ============
+    forgotPassword = catchAsync(async (req, res, next) => {
+        // 1. Get User based on POST email
+        const user = await Users.findOne({
+            where: { email: req.body.email },
+        });
+    
+        if (!user) {
+            return next(new AppError("There is no user with that email address.", 404));
+        }
+        console.log(user instanceof Users);
+        // 2. Generate the random reset token
+        const resetToken = user.createPasswordResetToken();
+        await user.save({ validate: false }); // Save the reset token and expiry to the DB
+    
+        // 3. Send it to user's email
+        const resetURL = `${req.protocol}://${req.get("host")}/api/v1/users/resetPassword/${resetToken}`;
+        const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email.`;
+    
+        try {
+            await sendEmail({
+                email: user.email,
+                subject: "Your password reset token (valid for 10 min)",
+                message,
+            });
+    
+            res.status(200).json({
+                status: "success",
+                message: "Token sent to email",
+            });
+        } catch (error) {
+            user.passwordResetToken = undefined;
+            user.passwordResetExpires = undefined;
+            await user.save({ validate: false }); // Save without the token if email fails
+    
+            return next(new AppError("There was an error sending the email. Try again later!", 500));
+        }
+    });
+    
+    // ============ End Forgot Password controller   ============
+
 }
 
 module.exports = new AuthControllers();
