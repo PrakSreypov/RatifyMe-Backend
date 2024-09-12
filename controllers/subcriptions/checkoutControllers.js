6d22bcb4ce5e10ebe2db512525fd8d7590da0520
lyDiyamong <diyamong26@gmail.com>
2024-09-12 16:45:13 +0700

    test(19-checkoutSession): testing checkout api

--- controllers/subcriptions/checkoutControllers.js
+++ controllers/subcriptions/checkoutControllers.js
@@ -5,15 +5,22 @@ const ServicePlans = require('../../models/ServicePlans')
 
 const catchAsync = require("../../utils/catchAsync");
 const AppError = require("../../utils/appError");
-const { where } = require("sequelize");
 
 // POST request to create Stripe Checkout session
 exports.createCheckoutSession = catchAsync(async (req, res, next) => {
     const { institutionId } = req.body;
     const { servicePlanId } = req.params;
-    // Convert plan ID to Stripe price ID
-    const {stripeProductId : priceId} = await ServicePlans.findByPk(servicePlanId)
-    console.log(priceId);
+    // Fetch the price ID from the database
+    const servicePlan = await ServicePlans.findByPk(servicePlanId);
+    if (!servicePlan) {
+        return next(new AppError('Service Plan not found', 404));
+    }
+    // console.log(servicePlan);
+
+    const priceId = servicePlan.stripePriceId; // Ensure this is the correct field
+    if (!priceId) {
+        return next(new AppError('Price ID not found for the service plan', 400));
+    }
 
     // Create the Stripe Checkout session
     const session = await stripe.checkout.sessions.create({
