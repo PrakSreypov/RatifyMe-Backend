56a5df9b5fb105a5a923ae1d89db711c0a218d34
lyDiyamong <diyamong26@gmail.com>
2024-09-12 16:44:29 +0700

    change(19-checkoutSession): change folder name

--- routers/subscriptions/index.js
+++ routers/subscriptions/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const checkoutRouters = require("./checkoutRouters");
 
-router.use("/subscribe/:servicePlanId", checkoutRouters);
+router.use("/subscribe/", checkoutRouters);
 // router.use("/checkoutSession", checkoutRouters);
 
 module.exports = router;
