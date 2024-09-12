b71861ac6f1d230d2638e95027c0c48dfac80fe4
Sreypovprak <sreypovprak88@gmail.com>
2024-09-12 20:51:13 +0700

    add(25-updatePassword): add global middleware for testing

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 // Error handling
 const AppError = require("./utils/appError");
 const globalErrorHandling = require("./utils/errorControllers");
 
 // Import the dynamic route loader
-const routers = require('./routers');
+const routers = require("./routers");
 
 const app = express();
 
 // Global Middleware
 app.use(express.json());
+app.use(cookieParser());
 app.use(helmet());
 app.use(cors());
 
@@ -21,8 +23,15 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
+// Test middleware
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    console.log(req.cookies);
+    next();
+});
+
 // Use the dynamically loaded routes
-app.use('/api/v1', routers);
+app.use("/api/v1", routers);
 
 // Handling Unhandled Routes
 app.all("*", (req, res, next) => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -37,6 +37,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     let token;
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
         token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookie.jwt) {
+        token = req.cookie.jwt;
     }
 
     if (!token) {
