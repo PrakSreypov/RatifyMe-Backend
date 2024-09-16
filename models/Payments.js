3c841b2034c6eaf9708af13eef4a80279eb1adb5
lyDiyamong <diyamong26@gmail.com>
2024-09-16 11:04:42 +0700

    refactor(07-model-properties): add start data and enddate default value change typo columns name

--- models/Payments.js
+++ models/Payments.js
@@ -1,75 +1,81 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../configs/database");
 
-const Payments = sequelize.define("Payments", {
-    id: {
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    subscriptionId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: "Subscriptions",
-            key: "id",
+const Payments = sequelize.define(
+    "Payments",
+    {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE",
-        validate: {
-            notNull: {
-                msg: "Subscription ID is required.",
+        subscriptionId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "Subscriptions",
+                key: "id",
             },
-            isInt: {
-                msg: "Subscription ID must be an integer.",
-            },
-        },
-    },
-    amount: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: "Amount is required.",
-            },
-            isDecimal: "Amount must be a decimal value.",
-            min: {
-                args: [0],
-                msg: "Amount must be greater than or equal to 0.",
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+            validate: {
+                notNull: {
+                    msg: "Subscription ID is required.",
+                },
+                isInt: {
+                    msg: "Subscription ID must be an integer.",
+                },
             },
         },
-    },
-    paymethMethod: {
-        type: DataTypes.STRING,
-    },
-    paymentDate: {
-        type: DataTypes.DATE,
-        validate: {
-            isDate: {
-                msg: "Payment date must be a valid date.",
-            },
-            isBeforeCurrentdate(value) {
-                if (new Date(value) > new Date()) {
-                    throw new Error("Payment date cannot be in the future.");
-                }
+        amount: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "Amount is required.",
+                },
+                isDecimal: "Amount must be a decimal value.",
+                min: {
+                    args: [0],
+                    msg: "Amount must be greater than or equal to 0.",
+                },
             },
         },
+        paymentMethod: {
+            type: DataTypes.STRING,
+        },
+        paymentDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW, 
+            validate: {
+                isDate: {
+                    msg: "Payment date must be a valid date.",
+                },
+                isBeforeCurrentdate(value) {
+                    if (new Date(value) > new Date()) {
+                        throw new Error("Payment date cannot be in the future.");
+                    }
+                },
+            },
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+        },
+        stripePriceId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
-    status: {
-        type: DataTypes.BOOLEAN,
+    {
+        indexes: [
+            {
+                unique: true,
+                fields: ["stripePriceId"],
+            },
+        ],
     },
-    stripePriceId : {
-        type : DataTypes.STRING,
-        allowNull : false
-    }
-}, {
-    indexes : [
-        {
-            unique : true,
-            fields : ['stripePriceId']
-        }
-    ]
-});
+);
 
 module.exports = Payments;
