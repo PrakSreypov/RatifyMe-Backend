c49b518c4f7d2de9700501016694baba830aeea5
SreangLyhour12 <slyhour69@gmail.com>
2024-09-16 18:11:24 +0700

    refactor(30-associateModels): add reference model of reference model

--- controllers/issuers/badgeClassControllers.js
+++ controllers/issuers/badgeClassControllers.js
@@ -1,8 +1,19 @@
 const BadgeClasses = require("../../models/BadgeClasses");
 const Issuers = require("../../models/Issuers");
+const Institutions = require("../../models/Institutions");
 
 const BaseControllers = require("../../utils/baseControllers");
 
-const badgeClassControllers = new BaseControllers(BadgeClasses, ["name"], [Issuers]);
+// Include both Issuers and Institutions in the association
+const badgeClassControllers = new BaseControllers(
+    BadgeClasses,
+    ["name"],
+    [
+        {
+            model: Issuers,
+            include: [{ model: Institutions }], // Include Institutions when fetching Issuers
+        },
+    ],
+);
 
 module.exports = badgeClassControllers;
