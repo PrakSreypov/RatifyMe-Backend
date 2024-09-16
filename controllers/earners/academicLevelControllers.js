25eaa1ca440c9b5622e2577381f38adee1e0a59b
Khun Malen <malen8622@gmail.com>
2024-09-16 10:39:12 +0700

    refactor(29-refactorAllControllers): change all earners's associate controllers

--- controllers/earners/academicLevelControllers.js
+++ controllers/earners/academicLevelControllers.js
@@ -2,6 +2,6 @@
 const AcademicLevels = require("../../models/AcademicLevels");
 
 const BaseControllers = require("../../utils/baseControllers");
-const academicLevelControllers = new BaseControllers(AcademicLevels);
+const academicLevelControllers = new BaseControllers(AcademicLevels, ["name"]);
 
 module.exports = academicLevelControllers;
--- controllers/earners/specializationControllers.js
+++ controllers/earners/specializationControllers.js
@@ -3,23 +3,5 @@ const FieldOfStudies = require("../../models/FieldOfStudies");
 
 const BaseControllers = require("../../utils/baseControllers");
 
-const catchAsync = require("../../utils/catchAsync");
-class SpecializationsControllers extends BaseControllers {
-    constructor() {
-        super(Specializations, [FieldOfStudies]);
-    }
-
-    getAll = catchAsync(async (req, res, next) => {
-        const specializations = await Specializations.findAll({
-            include: [{ model: FieldOfStudies }],
-        });
-
-        res.status(200).json({
-            status: "success",
-            results: specializations.length,
-            data: specializations,
-        });
-    });
-}
-
-module.exports = new SpecializationsControllers();
+const specializationsControllers = new BaseControllers(Specializations,["name"], [FieldOfStudies])
+module.exports = specializationsControllers;
