7964a896d5498b1027523ec64d31fd4ff245ed74
lyDiyamong <diyamong26@gmail.com>
2024-09-17 18:33:55 +0700

    add(27-webhook) : add bulk dummy data

--- data/institutionsData.js
+++ data/institutionsData.js
@@ -6,31 +6,28 @@ const institutions = [
         userId: 101,
         name: 'Tech Innovations Inc.',
         bio: 'A leading institution in technology and innovation, dedicated to fostering creativity and entrepreneurship.',
-        email: 'info@techinnovations.com',
+        email: 'info@techinnovationss.com',
         phoneNumber: '1234567890',
         websiteUrl: 'https://www.techinnovations.com',
         profileImage: 'https://www.techinnovations.com/images/profile.jpg',
-        stripeCustomerId: 'cus_1234567890'
     },
     {
         userId: 102,
         name: 'Health & Wellness Center',
         bio: 'Committed to improving health and wellness through education and community support.',
-        email: 'contact@healthwellness.org',
+        email: 'contact@healthwellnesss.org',
         phoneNumber: '0987654321',
         websiteUrl: 'https://www.healthwellness.org',
         profileImage: null,
-        stripeCustomerId: 'cus_0987654321'
     },
     {
         userId: 103,
         name: 'Creative Arts Academy',
         bio: 'An institution that nurtures artistic talent and promotes creative expression among students.',
-        email: 'admissions@creativeartsacademy.com',
+        email: 'admissions@creativeartsacademys.com',
         phoneNumber: '5551234567',
         websiteUrl: 'https://www.creativeartsacademy.com',
         profileImage: null,
-        stripeCustomerId: 'cus_5551234567'
     }
 ]
 
