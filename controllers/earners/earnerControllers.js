925d5fa48a6a887e1e24fed45ce0b558a70e6547
Khun Malen <malen8622@gmail.com>
2024-09-17 17:58:55 +0700

    refactor(30-fixSmallBug): refactor controller

--- controllers/earners/earnerControllers.js
+++ controllers/earners/earnerControllers.js
@@ -2,8 +2,32 @@ const Earners = require("../../models/Earners");
 const Users = require("../../models/Users");
 const Achievements = require("../../models/Achievements");
 const AcademicBackgrounds = require("../../models/AcademicBackgrounds");
-
+const BadgeClasses = require("../../models/BadgeClasses");
+const Roles = require("../../models/Roles");
 const BaseControllers = require("../../utils/baseControllers");
-
-const earnerControllers = new BaseControllers(Earners, [], [Users, Achievements, AcademicBackgrounds])
+const AcademicLevels = require("../../models/AcademicLevels")
+const Institutions = require("../../models/Institutions")
+const earnerControllers = new BaseControllers(
+    Earners,
+    [],
+    [
+        {
+            model: Users,
+            include: [Roles],
+        },
+        Achievements,
+        {
+            model: AcademicBackgrounds,
+            include: [AcademicLevels]
+        },
+        {
+            model: AcademicBackgrounds,
+            include: [Institutions]
+        },
+        {
+            model: Achievements,
+            include: [BadgeClasses],
+        },
+    ],
+);
 module.exports = earnerControllers;
