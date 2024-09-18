5a472c5b5d57258050ce27c906c3b6fc17c08244
lyDiyamong <diyamong26@gmail.com>
2024-09-18 13:49:33 +0700

    add(32-restoreController): add subscriptions routers

--- routers/subscriptions/index.js
+++ routers/subscriptions/index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 const checkoutRouters = require("./checkoutRouters");
 const servicePlanRouters = require("./servicePlanRouters");
+const subscriptionRouters = require("./subscriptionRouters");
 
 const webhookControllers = require("../../controllers/subcriptions/webhookControllers");
 
 // Subcribe session checkout Router
 router.use("/subscribe", checkoutRouters);
 
-// ServicePlan Router
-router.use("/servicePlan", servicePlanRouters);
+// ServicePlans Router
+router.use("/servicePlans", servicePlanRouters);
 
 // Webhook Router
 router.use('/webhook', express.json(), webhookControllers.webhook)
+
+// Subscriptions Router
+router.use("/", subscriptionRouters);
 
 module.exports = router;
