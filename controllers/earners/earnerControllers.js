c628450d6228e19dd4c1071972b19398e431f20b
Khun Malen <malen8622@gmail.com>
2024-09-19 15:06:42 +0700

    fix(30-fixSmallBug) : fix earnerControllers

--- controllers/earners/earnerControllers.js
+++ controllers/earners/earnerControllers.js
@@ -4,29 +4,26 @@ const Achievements = require("../../models/Achievements");
 const AcademicBackgrounds = require("../../models/AcademicBackgrounds");
 const BadgeClasses = require("../../models/BadgeClasses");
 const Roles = require("../../models/Roles");
+const Genders = require("../../models/Genders");
 const BaseControllers = require("../../utils/baseControllers");
-const AcademicLevels = require("../../models/AcademicLevels")
-const Institutions = require("../../models/Institutions")
+const AcademicLevels = require("../../models/AcademicLevels");
+const Institutions = require("../../models/Institutions");
+const AchievementTypes = require("../../models/AchievementTypes");
 const earnerControllers = new BaseControllers(
     Earners,
     [],
     [
         {
             model: Users,
-            include: [Roles],
-        },
-        Achievements,
-        {
-            model: AcademicBackgrounds,
-            include: [AcademicLevels]
+            include: [Roles, Genders],
         },
         {
             model: AcademicBackgrounds,
-            include: [Institutions]
+            include: [AcademicLevels, Institutions],
         },
         {
             model: Achievements,
-            include: [BadgeClasses],
+            include: [BadgeClasses, AchievementTypes],
         },
     ],
 );
