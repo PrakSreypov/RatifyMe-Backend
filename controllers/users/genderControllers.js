f0ef736933da27ea698b62ed78049a8249251600
Khun Malen <malen8622@gmail.com>
2024-09-21 17:56:12 +0700

    fix(34-addRecipient): fix genderControllers

--- controllers/users/genderControllers.js
+++ controllers/users/genderControllers.js
@@ -2,6 +2,6 @@ const Genders = require("../../models/Genders");
 
 const BaseControllers = require("../../utils/baseControllers");
 
-const genderControllers = new BaseControllers(Genders);
+const genderControllers = new BaseControllers(Genders, []);
 
 module.exports = genderControllers;
