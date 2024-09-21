876ba4c6ef9fed52cafbc9bb1102375724e9c038
Khun Malen <malen8622@gmail.com>
2024-09-21 17:52:23 +0700

    fix(34-addRecipient): fix achievementControllers

--- controllers/issuers/achievementControllers.js
+++ controllers/issuers/achievementControllers.js
@@ -1,13 +1,19 @@
 const BadgeClasses = require("../../models/BadgeClasses");
 const Achievements = require("../../models/Achievements");
 const AchievementTypes = require("../../models/AchievementTypes");
-
+const Issuers = require("../../models/Issuers");
 const BaseControllers = require("../../utils/baseControllers");
 
 const achievementControllers = new BaseControllers(
     Achievements,
     [],
-    [BadgeClasses, AchievementTypes],
+    [
+        {
+            model: BadgeClasses,
+            include: Issuers,
+        },
+        AchievementTypes,
+    ],
 );
 
 module.exports = achievementControllers;
