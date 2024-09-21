b3d4a434b2e020091fb4ea8d49292513502f8240
Khun Malen <malen8622@gmail.com>
2024-09-21 18:04:28 +0700

    add(34-addRecipient): add new InstitutionStat Router

--- routers/institutions/index.js
+++ routers/institutions/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const institutionRouters = require("./institutionRouters");
+const institutionStatsRouter = require("./InstitutionStatsRouters")
 
+router.use("/institutionStats", institutionStatsRouter )
 router.use("/", institutionRouters);
 
 module.exports = router;
