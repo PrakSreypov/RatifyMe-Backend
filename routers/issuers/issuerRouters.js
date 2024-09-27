f6ca736c0c34fa919c162b7246a948deae824d47
SreangLyhour12 <slyhour69@gmail.com>
2024-09-27 17:51:19 +0700

    add(35-sendBadgeEarner): add method to send badge to earner through achievement

--- routers/issuers/issuerRouters.js
+++ routers/issuers/issuerRouters.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const issuerControllers = require("../../controllers/issuers/issuerControllers");
+const sendBadgeControllers = require("../../controllers/issuers/sendBadgeControllers");
 
 router.route("/").get(issuerControllers.getAll).post(issuerControllers.createOne);
 
@@ -10,5 +11,7 @@ router
     .get(issuerControllers.getOne)
     .patch(issuerControllers.updateOne)
     .delete(issuerControllers.deleteOne);
+
+router.route("/sendBadge/:id").patch(sendBadgeControllers.sendBadges);
 
 module.exports = router;
