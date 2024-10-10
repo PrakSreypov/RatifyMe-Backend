418285401f929e5c5c95fd217a6f3ea4d3ea13f9
lyDiyamong <diyamong26@gmail.com>
2024-10-10 16:27:28 +0700

    change file path case

--- routers/institutions/index.js
+++ routers/institutions/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const institutionRouters = require("./institutionRouters");
-const institutionStatsRouter = require("./institutionStatsRouters")
+const institutionStatsRouter = require("./InstitutionStatsRouters")
 
 router.use("/institutionStats", institutionStatsRouter )
 router.use("/", institutionRouters);
