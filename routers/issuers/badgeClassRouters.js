f77d926f3d1d5f38a67593ba4489d47900e81a53
RothaSAMON <samonrotha@gmail.com>
2024-10-10 09:37:42 +0700

    add(42-uplaodBadge): added Edit controller of badge

--- routers/issuers/badgeClassRouters.js
+++ routers/issuers/badgeClassRouters.js
@@ -4,10 +4,12 @@ const {upload} = require("../../app")
 
 const badgeClassesControllers = require("../../controllers/issuers/badgeClassControllers");
 const addBadgesControllers = require("../../controllers/issuers/addBadgeClassControllers");
+const editBadgesControllers = require("../../controllers/issuers/editBadgeClassControllers");
 
 router.route("/").get(badgeClassesControllers.getAll).post(badgeClassesControllers.createOne);
 
 router.route("/addBadge").post(upload.single("badgeFile"), addBadgesControllers.addBadgeClass);
+router.route("/editBadge/:badgeId").patch(upload.single("badgeFile"), editBadgesControllers.editBadgeClass);
 
 
 router.route("/earner/:earnerId").get(badgeClassesControllers.getBadgeClassesByEarnerId);
