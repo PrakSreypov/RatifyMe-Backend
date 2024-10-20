d454cf610be4dc910a0caba78d0a3a7c063c1376
Sreypovprak <sreypovprak88@gmail.com>
2024-10-20 11:02:44 +0700

    refactor(44-emailVerification): refactor invite user logic when they already have a specific role

--- services/codeInvitationServices.js
+++ services/codeInvitationServices.js
@@ -52,29 +52,30 @@ class CodeInvitationService {
             return next(new AppError("This email has already been invited."));
         }
 
-        const existAccount = await Users.findOne({ where: { email: email, isVerified: 1 } });
+        // const existAccount = await Users.findOne({ where: { email: email, isVerified: 1 } });
 
-        if (existAccount) {
-            let errorMessage;
+        // if (existAccount) {
+        //     let errorMessage;
 
-            // Determine the error message based on the roleId
-            switch (existAccount.roleId) {
-                case 1:
-                case 2:
-                    errorMessage = "Account already has a specific role";
-                    break;
-                case 3:
-                    errorMessage = "Account already an issuer";
-                    break;
-                case 4:
-                    errorMessage = "Account already an earner";
-                    break;
-                default:
-                    errorMessage = "Unable to invite this account!";
-            }
+        //     // Determine the error message based on the roleId
+        //     switch (existAccount.roleId) {
+        //         case 1:
+        //         case 2:
+        //             errorMessage = "Account already has a specific role";
+        //             break;
+        //         case 3:
+        //             errorMessage = "Account already an issuer";
+        //             break;
+        //         case 4:
+        //             const 
+        //             errorMessage = "Account already an earner";
+        //             break;
+        //         default:
+        //             errorMessage = "Unable to invite this account!";
+        //     }
 
-            return next(new AppError(errorMessage));
-        }
+        //     return next(new AppError(errorMessage));
+        // }
 
         // Create an invitation in the database
         const guest = await InviteUsers.create({
