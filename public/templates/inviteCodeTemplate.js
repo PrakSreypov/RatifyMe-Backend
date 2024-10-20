e6d28d9d240cd8f5cbfb3118630c4ec5813f0e2e
Sreypovprak <sreypovprak88@gmail.com>
2024-10-20 11:17:50 +0700

    refactor(44-emailVerification): modified code invitation template

--- public/templates/inviteCodeTemplate.js
+++ public/templates/inviteCodeTemplate.js
@@ -5,43 +5,53 @@ exports.inviteCodeTemplate = `
         <head>
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Invitation to Join Badge Platform</title>
+            <title>Verify Your Email</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link
+                href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+                rel="stylesheet"
+            />
             <style>
                 body {
-                    font-family: Arial, sans-serif;
-                    background-color: #f7f7f7;
+                    font-family: "Roboto", sans-serif;
+                    background-color: #f6f9fa;
                     margin: 0;
                     padding: 0;
+                    color: #000;
                 }
-                p {
-                    color: #000 !important;
+                .box {
+                    background-color: #f6f9fa;
+                    width: 100%;
+                    display: flex;
+                    align-items: center;
                 }
                 .container {
                     width: 100%;
                     max-width: 600px;
+                    max-height: 700px;
                     margin: 20px auto;
-                    background-color: #ffffff;
-                    border-radius: 8px;
+                    background-color: #fff;
+                    border-radius: 12px;
                     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+                    padding: 24px 40px;
                 }
                 .header {
-                    background-color: #1A87EC;
-                    padding: 20px;
+                    padding: 24px;
                     border-radius: 8px 8px 0 0;
                     color: #ffffff;
                     text-align: center;
                 }
-                .header h1 {
-                    margin: 0;
-                    font-size: 24px;
-                }
                 .content {
                     padding: 20px;
                 }
-                .content p {
+                p {
                     font-size: 16px;
-                    line-height: 1.6;
-                    margin: 20px 0;
+                    line-height: 1.5;
+                    color: #000;
+                }
+                .content .greeting {
+                    font-size: 18px;
                 }
                 .code-block {
                     background-color: #f0f0f0;
@@ -53,18 +63,25 @@ exports.inviteCodeTemplate = `
                     font-weight: bold;
                     letter-spacing: 1.5px;
                 }
-                .cta-button {
-                    display: block;
-                    width: 200px;
-                    margin: 30px auto;
-                    padding: 10px 20px;
-                    background-color: #1A87EC;
-                    color: #ffffff !important;
-                    text-align: center;
+                .link-platform,
+                .email {
                     text-decoration: none;
-                    border-radius: 5px;
+                    color: #1a87ec;
                     font-size: 18px;
                 }
+                .cta-button {
+                        display: block;
+                        width: 200px;
+                        margin: 30px auto;
+                        padding: 10px 20px;
+                        background-color: #1A87EC;
+                        color: #ffffff !important;
+                        text-align: center;
+                        text-decoration: none;
+                        border-radius: 5px;
+                        font-size: 18px;
+                    }
+
                 .footer {
                     text-align: center;
                     padding: 20px;
@@ -74,26 +91,33 @@ exports.inviteCodeTemplate = `
             </style>
         </head>
         <body>
-            <div class="container">
-                <div class="header">
-                    <h1>Invitation to Join [Institution Name]</h1>
-                </div>
-                <div class="content">
-                    <p>
-                        You’ve been invited by <strong>[Institution Name]</strong> to join their
-                        badge-issuing program on the <strong>[Badge Platform]</strong>.
-                    </p>
-                    <p>To join, please use the following invitation code when signing up:</p>
-                    <div class="code-block">[INVITE CODE]</div>
-                    <p>Click the button below to get started and create your account.</p>
-                    <a href="[SIGNUP_LINK]" class="cta-button">Create Account</a>
-                    <p>If you have any questions or need assistance, feel free to contact us.</p>
-                    <p>Best regards,<br />The RatifyMe Team</p>
-                </div>
-                <div class="footer">
-                    <p>&copy; 2024 [Badge Platform Name]. All rights reserved.</p>
+            <div class="box">
+                <div class="container">
+                    <div class="header">
+                        <img
+                            src="https://ratifyme.s3.ap-southeast-2.amazonaws.com/Logo/RatfiyME.png"
+                            width="40%"
+                        />
+                    </div>
+                    <div class="content">
+                        <p class="greeting">Hello,</p>
+                        <p>
+                            You’ve been invited to join the
+                            badge-issuing program on the <strong>[Badge Platform]</strong>.
+                        </p>
+                        <p>To join, please use the following invitation code when signing up:</p>
+                        <div class="code-block">[INVITE CODE]</div>
+                        <p>Click the button below to get started and create your account.</p>
+                        <a href="[SIGNUP_LINK]" class="cta-button">Create Account</a>
+                        <p>If you have any questions or need assistance, feel free to contact us.</p>
+                        <p>Best regards,<br /><b>The RatifyMe Team</b></p>
+                    </div>
+                    <div class="footer">
+                        <p>&copy; 2024 RatifyMe. All rights reserved.</p>
+                    </div>
                 </div>
             </div>
         </body>
     </html>
+
 `;
