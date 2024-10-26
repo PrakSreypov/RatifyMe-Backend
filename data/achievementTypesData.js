09b44e2d7ec16c9ce676973b83e3529f641fcb51
Sreypovprak <sreypovprak88@gmail.com>
2024-10-26 18:42:54 +0700

    fix(67-badgeData): modified data in achievement type data

--- data/achievementTypesData.js
+++ data/achievementTypesData.js
@@ -8,7 +8,7 @@ const achievementTypesData = [
             "A general term for any accomplishment or milestone recognized for completing a specific task or reaching a goal.",
     },
     {
-        name: "ApprenticeshipCertificate",
+        name: "Apprenticeship Certificate",
         description:
             "A certification awarded upon completing an apprenticeship program, indicating the individual has acquired practical skills and experience in a particular trade or profession.",
     },
@@ -23,7 +23,7 @@ const achievementTypesData = [
             "A certificate or acknowledgment given upon the successful completion of a specific academic or professional task or project.",
     },
     {
-        name: "AssociateDegree",
+        name: "Associate Degree",
         description:
             "An academic degree awarded upon the completion of a two-year program at a community college or junior college, often as a precursor to a bachelor's degree.",
     },
@@ -38,7 +38,7 @@ const achievementTypesData = [
             "A symbolic token or digital marker awarded for completing certain tasks, achieving milestones, or demonstrating skills, often used in educational or professional settings.",
     },
     {
-        name: "BachelorDegree",
+        name: "Bachelor Degree",
         description:
             "An undergraduate academic degree awarded after the completion of a college or university program, typically lasting four years, in a specific field of study.",
     },
@@ -48,7 +48,7 @@ const achievementTypesData = [
             "A document certifying the completion of a specific course or program, recognizing the acquisition of certain skills or knowledge.",
     },
     {
-        name: "CertificateOfCompletion",
+        name: "Certificate Of Completion",
         description:
             "A document awarded upon finishing a course or training program, signifying that the individual has completed all required components.",
     },
@@ -58,7 +58,7 @@ const achievementTypesData = [
             "A formal recognition, usually involving an examination or assessment, that an individual has met specific professional or educational standards in a particular field.",
     },
     {
-        name: "CommunityService",
+        name: "Community Service",
         description:
             "An acknowledgment for voluntary work or contributions made to benefit the community, demonstrating commitment to civic engagement and social responsibility.",
     },
@@ -73,7 +73,7 @@ const achievementTypesData = [
             "A series of instructional sessions or lessons on a particular subject, with a certificate or acknowledgment given upon successful completion.",
     },
     {
-        name: "CoCurricular",
+        name: "Co-Curricular",
         description:
             "A certificate or recognition for activities that complement the main curriculum, such as clubs, sports, or other extracurricular activities that contribute to personal development.",
     },
@@ -88,7 +88,7 @@ const achievementTypesData = [
             "An official document certifying the completion of a specific educational program, often awarded at the end of secondary education or higher education.",
     },
     {
-        name: "DoctoralDegree",
+        name: "Doctoral Degree",
         description:
             "The highest level of academic degree, awarded upon completion of advanced research and study, typically involving a dissertation or thesis in a specialized field.",
     },
@@ -98,12 +98,12 @@ const achievementTypesData = [
             "A recognition for hands-on, practical experience gained through direct engagement in a particular field of study or profession, often required as part of academic programs.",
     },
     {
-        name: "GeneralEducationDevelopment",
+        name: "General Education Development",
         description:
             "A certificate awarded to individuals who have demonstrated high school-level academic skills through the General Education Development (GED) test, often serving as a high school equivalency credential.",
     },
     {
-        name: "JourneymanCertificate",
+        name: "Journeyman Certificate",
         description:
             "A certification indicating that an individual has completed an apprenticeship and achieved proficiency in a trade, qualifying them as a skilled worker.",
     },
@@ -118,37 +118,37 @@ const achievementTypesData = [
             "An acknowledgment of joining a professional organization, society, or association, often providing access to resources, networks, and professional development opportunities.",
     },
     {
-        name: "ProfessionalDoctorate",
+        name: "Professional Doctorate",
         description:
             "An advanced academic degree that focuses on professional practice and applied research in a specific field, such as a Doctor of Medicine (MD) or Doctor of Business Administration (DBA).",
     },
     {
-        name: "QualityAssuranceCredential",
+        name: "Quality Assurance Credential",
         description:
             "A certification demonstrating expertise in quality assurance practices and standards, often relevant in fields such as manufacturing, software development, or project management.",
     },
     {
-        name: "MasterCertificate",
+        name: "Master Certificate",
         description:
             "A specialized credential recognizing advanced knowledge or skills in a specific area, often part of a professional development program or certification track.",
     },
     {
-        name: "MasterDegree",
+        name: "Master Degree",
         description:
             "An advanced academic degree awarded after completing graduate-level coursework, typically requiring one to two years of study beyond a bachelor's degree, in a specific field of study.",
     },
     {
-        name: "MicroCredential",
+        name: "Micro Credential",
         description:
             "A short, focused certification that verifies specific skills or knowledge in a particular area, often used to recognize achievements in continuing education or professional development.",
     },
     {
-        name: "ResearchDoctorate",
+        name: "Research Doctorate",
         description:
             "The highest academic degree, focusing on original research and contributing new knowledge to a specific field, typically culminating in a dissertation or thesis.",
     },
     {
-        name: "SecondarySchoolDiploma",
+        name: "Secondary School Diploma",
         description:
             "A certificate awarded upon completing secondary or high school education, signifying that the individual has met the required academic standards for graduation.",
     },
