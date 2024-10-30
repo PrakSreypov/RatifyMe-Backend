6217c073f6c134182714b1237c121a5022efdc2a
RothaSAMON <samonrotha@gmail.com>
2024-10-30 10:57:40 +0700

    fix(1.1) : cancel the reqiure of the Tags

--- models/BadgeClasses.js
+++ models/BadgeClasses.js
@@ -46,13 +46,6 @@ const BadgeClasses = sequelize.define(
         },
         tags: {
             type: DataTypes.STRING,
-            allowNull: true,
-            validate: {
-                len: {
-                    args: [0, 255],
-                    msg: "Tags cannot exceed 255 characters",
-                },
-            },
         },
         startedDate: {
             type: DataTypes.DATE,
